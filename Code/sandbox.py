4b9e1fda2f8e95d48fec495efab3a69a93fe8bce
V Castilho <vcastilho3@gatech.edu>
2023-07-16 20:36:07 +0200

    add elegance to code.

--- Code/sandbox.py
+++ Code/sandbox.py
@@ -12,69 +12,54 @@ explainable_variables = ['s0', 'w0', 'blsame_1', 'slsame_1', 'wlsame_1', 'blsame
 # Read df
 df = pd.read_csv(os.path.join(processed_results_dir, "df.csv"), index_col=0)
 
+
+# helper function
+def print_heatmap(df, title:str):
+    # correlation after removing multicollinearity
+    cols_sorted = df.columns.sort_values()
+    df = df[cols_sorted]
+    corr_matrix = df.corr()
+
+    plt.figure(figsize=(8, 6))  # Adjust the figure size as per your preference
+
+    sns.heatmap(corr_matrix, annot=False, cmap="coolwarm", linewidths=0.5, cbar=True)
+
+    plt.xticks(rotation=90, fontsize=8)  # Rotate and adjust the font size of the X-axis labels
+    plt.yticks(fontsize=8)  # Adjust the font size of the Y-axis labels
+    plt.title(title, fontsize=10)  # Adjust the font size of the title
+    plt.show()
+
+
+
 # Train model on horizon 20
 subset_data = df[df['horizon_label'] == 20]
 X = subset_data[explainable_variables]
 y = subset_data[target_variable]
 
 
-# Remove the columns that are not needed or pending further cleaning
-# print("===========", horizon, "===========")
-# print("Current row count:", len(X))
-# remove = ['vol_0_1', 'vol_0_2', 'vol_0_3', 'avg-USD-iother_01', 'rate-USD-iother_01']
-# X = X.drop(remove, axis=1)
-
 # Remove the rows that contain null values
 null_rows = X[X.isnull().any(axis=1)].index
 X = X.drop(null_rows, axis=0)
 y = y.drop(null_rows, axis=0)
 
-# print("New row count:", len(X))
-
-
 # Fit the OLS model
 model = sm.OLS(y, X)
 results = model.fit()
 
-
 # Retrieve the R-squared value
 r_squared = results.rsquared
-
 
 # identify multicollinearity
 vif = pd.DataFrame()
 vif["Variable"] = X.columns
 vif["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
 
-
 # remove cols with multicollinearity
 X_red = X.drop(columns = vif[vif['VIF'] > 10]['Variable'])
 
-# correlation before removing multicollinearity
-cols_sorted = X.columns.sort_values()
-X = X[cols_sorted]
-corr_matrix = X.corr()
+title_before = "Correlation Matrix of Independent Variables - Before Multicollinearity"
+title_after = "Correlation Matrix of Independent Variables - After Multicollinearity"
 
-plt.figure(figsize=(8, 6))  # Adjust the figure size as per your preference
+print_heatmap(X, title_before)
 
-sns.heatmap(corr_matrix, annot=False, cmap="coolwarm", linewidths=0.5, cbar=False)
-
-plt.xticks(rotation=90, fontsize=8)  # Rotate and adjust the font size of the X-axis labels
-plt.yticks(fontsize=8)  # Adjust the font size of the Y-axis labels
-plt.title("Correlation Matrix of Independent Variables - Before Multicollinearity", fontsize=10)  # Adjust the font size of the title
-plt.show()
-
-
-# correlation after removing multicollinearity
-cols_sorted = X_red.columns.sort_values()
-X_red = X_red[cols_sorted]
-corr_matrix = X_red.corr()
-
-plt.figure(figsize=(8, 6))  # Adjust the figure size as per your preference
-
-sns.heatmap(corr_matrix, annot=False, cmap="coolwarm", linewidths=0.5, cbar=False)
-
-plt.xticks(rotation=90, fontsize=8)  # Rotate and adjust the font size of the X-axis labels
-plt.yticks(fontsize=8)  # Adjust the font size of the Y-axis labels
-plt.title("Correlation Matrix of Independent Variables - After Multicollinearity", fontsize=10)  # Adjust the font size of the title
-plt.show()
+print_heatmap(X_red, title_after)
